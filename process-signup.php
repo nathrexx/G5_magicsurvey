9d8b80a71db69e2b191d09fb6cbcc28496155770
Daniel4930 <phule4108@gmail.com>
2023-05-10 07:59:49 -0700

    some changes

--- process-signup.php
+++ process-signup.php
@@ -1,35 +1,31 @@
 <?php
-
-$password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
+$password_hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
 
 $mysqli = require __DIR__ . "/database.php";
 
+// Retrieve user input data
+$first_name = $_POST['first_name'];
+$last_name = $_POST['last_name'];
+$email = $_POST['email'];
+$phone_number = $_POST['phone_number'];
+
+// Sanitize and validate input data
+$first_name = mysqli_real_escape_string($mysqli, $first_name);
+$last_name = mysqli_real_escape_string($mysqli, $last_name);
+$email = mysqli_real_escape_string($mysqli, $email);
+$phone_number = mysqli_real_escape_string($mysqli, $phone_number);
+
+// Construct an SQL query
 $sql = "INSERT INTO users (first_name, last_name, email, phone_number, password_hash)
-        VALUES (?, ?, ?, ?, ?)";
-        
-$stmt = $mysqli->stmt_init();
+        VALUES ('$first_name', '$last_name', '$email', '$phone_number', '$password_hash')";
 
-if ( ! $stmt->prepare($sql)) {
-    die("SQL error: " . $mysqli->error);
-}
-
-$stmt->bind_param("sssss",
-                  $_POST["first_name"],
-                  $_POST["last_name"],
-                  $_POST["email"],
-                  $_POST["phone_number"],
-                  $password_hash);
-                  
-if ($stmt->execute()) {
-
-    header("Location: homepage.html");
-    exit;
-    
+// Construct an SQL query
+if (mysqli_query($mysqli, $sql)) {
+    echo "Data inserted successfully";
 } else {
-    
-    if ($mysqli->errno === 1062) {
-        die("email already taken");
-    } else {
-        die($mysqli->error . " " . $mysqli->errno);
-    }
+    echo "Error: " . mysqli_error($mysqli);
 }
+
+// Close the database connection
+$mysqli->close();
+?>
